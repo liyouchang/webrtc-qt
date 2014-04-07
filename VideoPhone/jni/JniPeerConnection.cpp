51c372be692db1d936e37eb11644ff6bccc6f09c
lihaote <haoteli@gmail.com>
2014-04-07 15:56:36 +0800

    change jinpeerconnection

--- VideoPhone/jni/JniPeerConnection.cpp
+++ VideoPhone/jni/JniPeerConnection.cpp
@@ -16,17 +16,24 @@ JniPeerConnection::~JniPeerConnection() {
 
 }
 
+struct SendToPeerData : public talk_base::MessageData{
+	std::string peer_id;
+	std::string message;
+	SendToPeerData(std::string pid,std::string msg):peer_id(pid),message(msg){}
+};
 bool JniPeerConnection::SendToPeer(const std::string& peer_id,
 		const std::string& message) {
-	LOGI("JniPeerConnection::SendToPeer");
+	LOGI("JniPeerConnection::SendToPeer----");
+
 
 //	bool ret =  jni_thread->Invoke<bool>(
 //			talk_base::Bind(&JniPeerConnection::SendToPeer_jni, this,peer_id,message));
-	bool ret =  JniUtil::GetInstance()->JniSendToPeer(peer_id.c_str(),message.c_str());
+//	bool ret =  JniUtil::GetInstance()->JniSendToPeer(peer_id.c_str(),message.c_str());
 
-	LOGI("jni invoke %d",ret);
+	jni_thread->Post(this,MSG_SENDTOPEER,new SendToPeerData(peer_id,message));
+	//LOGI("jni invoke %d",ret);
 
-	return ret;
+	return true;
 }
 
 bool JniPeerConnection::SendToPeer_jni(const std::string& peer_id,
@@ -35,6 +42,17 @@ bool JniPeerConnection::SendToPeer_jni(const std::string& peer_id,
 
 	return JniUtil::GetInstance()->JniSendToPeer(peer_id.c_str(),message.c_str());
 }
+
+void JniPeerConnection::OnMessage(talk_base::Message* msg) {
+	LOGI("JniPeerConnection::OnMessage");
+
+	if(MSG_SENDTOPEER == msg->message_id){
+		SendToPeerData * pd = static_cast<SendToPeerData *>(msg->pdata);
+		SendToPeer_jni(pd->peer_id,pd->message);
+		delete msg->pdata;
+	}
+}
+
 
 
 
--- VideoPhone/jni/JniPeerConnection.h
+++ VideoPhone/jni/JniPeerConnection.h
@@ -10,9 +10,12 @@
 
 #include "libjingle_app/PeerConnectionClinetInterface.h"
 #include "talk/base/thread.h"
-
-class JniPeerConnection : public kaerp2p::PeerConnectionClientInterface {
+#include "talk/base/messagehandler.h"
+class JniPeerConnection : public kaerp2p::PeerConnectionClientInterface ,public talk_base::MessageHandler{
 public:
+	enum{
+		MSG_SENDTOPEER
+	};
 	JniPeerConnection();
 	virtual ~JniPeerConnection();
 
@@ -20,6 +23,7 @@ public:
 protected:
     bool SendToPeer_jni(const std::string &peer_id, const std::string& message);
     talk_base::Thread * jni_thread;
+    virtual void OnMessage(talk_base::Message * msg);
 };
 
 #endif /* JNIPEERCONNECTION_H_ */
