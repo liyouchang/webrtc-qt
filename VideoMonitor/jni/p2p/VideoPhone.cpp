07b988da257538bf1e01d9e795cd3bb462e3b509
liyouchang <haoteli@gmail.com>
2014-05-16 13:37:30 +0800

    android build avi record

--- VideoMonitor/jni/p2p/VideoPhone.cpp
+++ VideoMonitor/jni/p2p/VideoPhone.cpp
@@ -13,82 +13,119 @@ KeJniTunnelClient * client = NULL;
 JniPeerConnection * jniPeer = NULL;
 
 jint naInitialize(JNIEnv *env, jobject thiz, jstring cbClass) {
-	LOGI("1. naInitialize()");
+    LOGI("1. naInitialize()");
 
-	env->GetJavaVM(&JniUtil::GetInstance()->g_vm_);
-	JniUtil::GetInstance()->g_obj_ = env->NewGlobalRef(thiz);
+    env->GetJavaVM(&JniUtil::GetInstance()->g_vm_);
+    JniUtil::GetInstance()->g_obj_ = env->NewGlobalRef(thiz);
 
-	jniPeer = new JniPeerConnection();
-	client = new KeJniTunnelClient();
+    jniPeer = new JniPeerConnection();
+    client = new KeJniTunnelClient();
 
-	client->Init(jniPeer);
+    client->Init(jniPeer);
 
-	return 0;
+    return 0;
 }
 jint naTerminate(JNIEnv *env, jobject thiz) {
-	LOGI("2. naTerminate()");
-	if (jniPeer) {
-		delete jniPeer;
-		jniPeer = NULL;
-	}
-	if (client) {
-		delete client;
-		client = NULL;
-	}
+    LOGI("2. naTerminate()");
+    if (jniPeer) {
+        delete jniPeer;
+        jniPeer = NULL;
+    }
+    if (client) {
+        delete client;
+        client = NULL;
+    }
 
-	return 0;
+    return 0;
 }
 
 jint naOpenTunnel(JNIEnv *env, jobject thiz, jstring peer_id) {
-	LOGI("3. naOpenTunnel()");
-	if (client == NULL) {
-		return -1;
-	}
+    LOGI("3. naOpenTunnel()");
+    if (client == NULL) {
+        return -1;
+    }
 
-	const char * pid = env->GetStringUTFChars(peer_id, NULL);
+    const char * pid = env->GetStringUTFChars(peer_id, NULL);
 
-	return client->OpenTunnel(pid);
+    return client->OpenTunnel(pid);
 }
-jint naMessageFromPeer(JNIEnv *env, jobject thiz, jstring peer_id, jstring message){
-		if (jniPeer == NULL) {
-			return -1;
-		}
-		const char * pid = env->GetStringUTFChars(peer_id, NULL);
-		const char * pMsg = env->GetStringUTFChars(message,NULL);
-		jniPeer->OnMessageFromPeer(pid,pMsg);
-		return 0;
+jint naMessageFromPeer(JNIEnv *env, jobject thiz,
+                       jstring peer_id, jstring message){
+    if (jniPeer == NULL) {
+        return -1;
+    }
+    const char * pid = env->GetStringUTFChars(peer_id, NULL);
+    const char * pMsg = env->GetStringUTFChars(message,NULL);
+    jniPeer->OnMessageFromPeer(pid,pMsg);
+    env->ReleaseStringUTFChars(peer_id,pid);
+    env->ReleaseStringUTFChars(message,pMsg);
+    return 0;
 
 }
 jint naCloseTunnel(JNIEnv *env, jobject thiz, jstring peer_id) {
-	LOGI("4. naCloseTunnel()");
-	if (client == NULL) {
-		return -1;
-	}
-	const char * pid = env->GetStringUTFChars(peer_id, NULL);
-	return client->CloseTunnel(pid);
+    LOGI("4. naCloseTunnel()");
+    if (client == NULL) {
+        return -1;
+    }
+    const char * pid = env->GetStringUTFChars(peer_id, NULL);
+    return client->CloseTunnel(pid);
 }
 
 jint naAskMediaData(JNIEnv *env, jobject thiz, jstring peer_id) {
-	LOGI("5. naAskMediaData()");
-	if (client == NULL) {
-		return -1;
-	}
-	const char * pid = env->GetStringUTFChars(peer_id, NULL);
-	return client->StartPeerMedia(pid,2);
+    LOGI("5. naAskMediaData()");
+    if (client == NULL) {
+        return -1;
+    }
+    const char * pid = env->GetStringUTFChars(peer_id, NULL);
+
+    client->StartPeerMedia(pid,2);
+
+    env->ReleaseStringUTFChars(peer_id,pid);
+    return 0;
+
 }
 
 
-jint naSendTalkData(JNIEnv *env, jobject thiz, jbyteArray talkBytes,jint dataLen) {
-	LOGI("5. naSendTalkData()");
-	if (client == NULL) {
-		return -1;
-	}
-    jbyte * talkData = env->GetByteArrayElements( talkBytes, 0);
+jint naSendTalkData(JNIEnv *env, jobject thiz,
+                    jbyteArray talkBytes,jint dataLen) {
+    LOGI("5. naSendTalkData()");
+    if (client == NULL){
+        return -1;
+    }
+    jbyte * talkData = env->GetByteArrayElements(talkBytes, 0);
     //jsize len = env->GetArrayLength(talkBytes);
 
-    client->SendTalkData( (char *)talkData,dataLen);
+    client->SendTalkData((char *)talkData,dataLen);
 
     env->ReleaseByteArrayElements(talkBytes,talkData,0);
+    return 0;
+}
+
+jint naStartPeerVideoCut(JNIEnv *env, jobject thiz,
+                         jstring peer_id,jstring filename){
+    if (client == NULL){
+        return -1;
+    }
+    const char *pid = env->GetStringUTFChars(peer_id, NULL);
+    const char *file = env->GetStringUTFChars(filename,NULL);
+
+    client->StartPeerVideoCut(pid,file);
+
+    env->ReleaseStringUTFChars(peer_id,pid);
+    env->ReleaseStringUTFChars(filename,file);
+    return 0;
+
+}
+
+jint naStopPeerVideoCut(JNIEnv *env, jobject thiz,jstring peer_id){
+    if (client == NULL){
+        return -1;
+    }
+    const char *pid = env->GetStringUTFChars(peer_id, NULL);
+
+    client->StopPeerVideoCut(pid);
+
+    env->ReleaseStringUTFChars(peer_id,pid);
     return 0;
 }
 
@@ -100,26 +137,31 @@ jint naSendTalkData(JNIEnv *env, jobject thiz, jbyteArray talkBytes,jint dataLen
 jint JNI_OnLoad(JavaVM * pVm, void * reserved) {
     talk_base::LogMessage::ConfigureLogging("tstamp thread info debug",NULL);
 
-	JNIEnv * env;
-	if (pVm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK) {
-		return -1;
-	}
+    JNIEnv * env;
+    if (pVm->GetEnv((void **) &env, JNI_VERSION_1_6) != JNI_OK) {
+        return -1;
+    }
 
-	JNINativeMethod nm[] = {
-			{ "naInitialize", "(Ljava/lang/String;)I", (void*) naInitialize },
-			{ "naTerminate", "()I", (void*) naTerminate },
-			{ "naOpenTunnel", "(Ljava/lang/String;)I", (void*) naOpenTunnel },
-			{ "naCloseTunnel", "(Ljava/lang/String;)I", (void*) naCloseTunnel },
-			{ "naAskMediaData", "(Ljava/lang/String;)I", (void*) naAskMediaData },
-			{ "naMessageFromPeer", "(Ljava/lang/String;Ljava/lang/String;)I", (void*) naMessageFromPeer },
-			{ "naSendTalkData", "([BI)I", (void*) naSendTalkData }
+    JNINativeMethod nm[] = {
+        { "naInitialize", "(Ljava/lang/String;)I", (void*) naInitialize },
+        { "naTerminate", "()I", (void*) naTerminate },
+        { "naOpenTunnel", "(Ljava/lang/String;)I", (void*) naOpenTunnel },
+        { "naCloseTunnel", "(Ljava/lang/String;)I", (void*) naCloseTunnel },
+        { "naAskMediaData", "(Ljava/lang/String;)I", (void*) naAskMediaData },
+        { "naMessageFromPeer", "(Ljava/lang/String;Ljava/lang/String;)I",
+          (void*) naMessageFromPeer },
+        { "naSendTalkData", "([BI)I", (void*) naSendTalkData },
+        { "naOpenTunnel", "(Ljava/lang/String;Ljava/lang/String;)I",
+          (void*) naStartPeerVideoCut },
+        { "naOpenTunnel", "(Ljava/lang/String;)I", (void*) naStopPeerVideoCut },
 
-	};
 
-	jclass cls = env->FindClass("com/video/play/TunnelCommunication");
-	env->RegisterNatives(cls, nm, NELEM(nm));
+    };
 
-	JniUtil::GetInstance()->g_vm_ = pVm;
+    jclass cls = env->FindClass("com/video/play/TunnelCommunication");
+    env->RegisterNatives(cls, nm, NELEM(nm));
 
-	return JNI_VERSION_1_6;
+    JniUtil::GetInstance()->g_vm_ = pVm;
+
+    return JNI_VERSION_1_6;
 }
